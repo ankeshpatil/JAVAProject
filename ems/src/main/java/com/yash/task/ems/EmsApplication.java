1341394ed343966023982deb1bdcb4cf90e408f6
Ankesh <ankeshpatil156@gmail.com>
2023-02-17 12:39:25 +0530

    Second Commit

--- ems/src/main/java/com/yash/task/ems/EmsApplication.java
+++ ems/src/main/java/com/yash/task/ems/EmsApplication.java
@@ -11,7 +11,7 @@ public class EmsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmsApplication.class, args);
-		System.out.println("Application Started.........");
+		System.out.println("Application Started..");
 	}
 
 }
